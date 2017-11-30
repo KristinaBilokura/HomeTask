b8c73a56b35ca98117b06101a8a95bfda3e54541
bilokura <kristinabilokura@gmail.com>
2017-11-30 16:07:50 +0200

    Number+

---
+++ src/com/epam/lab/TelephoneNumber/Founder.java
@@ -0,0 +1,36 @@
+package com.epam.lab.TelephoneNumber;
+
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Founder {
+    private static final Logger log = Logger.getLogger(LinkedList.class);
+
+    public static void main(String[] args) throws IOException {
+        File path = new File("number.txt");
+        findNumber(path);
+    }
+
+    private static void findNumber(File path) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line,line2;
+            while ((line = br.readLine()) != null) {
+                Pattern p = Pattern.compile("\\+300+?");
+                Matcher m = p.matcher(line);
+                if (m.find()) {
+                    System.out.println(line);
+                }
+            }
+
+            br.close();
+        }catch (Exception e) {
+            System.out.println("OOPS! File could not read!");
+        }
+
+    }
+}
