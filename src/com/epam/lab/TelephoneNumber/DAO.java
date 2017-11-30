d3952dc61e76fd5c22e8bb96cf256de460a1fce3
bilokura <kristinabilokura@gmail.com>
2017-11-30 16:40:44 +0200

    One new QUERY

--- src/com/epam/lab/TelephoneNumber/DAO.java
+++ src/com/epam/lab/TelephoneNumber/DAO.java
@@ -15,17 +15,29 @@ public class DAO {
             Class.forName(driver).newInstance();
             System.out.println("Connected successfully!");
             Person person = new Person();
-            gueryDept(con , person);
+            System.out.println("Find people with ukrainian numbers");
+            queryPerson(con , person);
+            System.out.println("Foreigners");
+            ifThereforeigners(con, person);
         }catch (Exception e) {
             System.out.println(e);
         }
         return null;
     }
-    public static void gueryDept(Connection con , Person person) throws Exception {
+    public static void queryPerson(Connection con , Person person) throws Exception {
         Statement statement = con.createStatement();
         ResultSet resultSet = statement.executeQuery("SELECT name,number FROM git.people where number like '+380%'");
         while (resultSet.next()) {
-            System.out.println("Name  : " + resultSet.getString(person.getName())+"Number "+ resultSet.getString(person.getNumber())
+            System.out.println("Name  : " + resultSet.getString(person.getName())+"Number :"+ resultSet.getString(person.getNumber())
+            ) ;
+        }
+        System.out.println("Department table!");
+    }
+    public static void ifThereforeigners(Connection con , Person person) throws Exception {
+        Statement statement = con.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT name,surname FROM git.people where decimile not like 'UA'");
+        while (resultSet.next()) {
+            System.out.println("Name: " + resultSet.getString(person.getName())+"Surname :"+ resultSet.getString(person.getSurname())
             ) ;
         }
         System.out.println("Department table!");
